96acbbf8e71ff11e8c26e6bd6a5ab2d8c660fd0b
agerard <antoine.gerard@intuitim.com>
2024-02-17 19:54:47 +0100

    adds clean up print.c

--- print_grid.c
+++ print_grid.c
@@ -11,22 +11,21 @@ void print_grid(int grid[4][4])
 	// Boucle sur chaque ligne de la grille en utilisant une boucle while
 	while (i < 4)
 	{
-	j = 0; // Initialise l'indice de la colonne à 0 pour chaque nouvelle ligne
+		j = 0; // Initialise l'indice de la colonne à 0 pour chaque nouvelle ligne
 
-	// Boucle sur chaque colonne de la ligne actuelle en utilisant une boucle while
-	while (j < 4)
-	{
-		num = grid[i][j] + '0'; // Convertit le nombre (int) en son équivalent caractère
-		write(1, &num, 1); // Affiche le caractère numérique
+		// Boucle sur chaque colonne de la ligne actuelle en utilisant une boucle while
+		while (j < 4)
+		{
+			num = grid[i][j] + '0'; // Convertit le nombre (int) en son équivalent caractère
+			write(1, &num, 1); // Affiche le caractère numérique
 
-		// Si ce n'est pas le dernier nombre de la ligne, ajoute un espace après
-		if (j < 4 - 1)
-			write(1, " ", 1); // Ajoute un espace entre les nombres pour une meilleure lisibilité
+			// Si ce n'est pas le dernier nombre de la ligne, ajoute un espace après
+			if (j < 4 - 1)
+				write(1, " ", 1); // Ajoute un espace entre les nombres pour une meilleure lisibilité
 
-		j++; // Incrémente l'indice de la colonne pour passer à la colonne suivante
-	}
-	write(1, "\n", 1); // Après avoir affiché tous les nombres de la ligne, ajoute un saut de ligne
-
-	i++; // Incrémente l'indice de la ligne pour passer à la ligne suivante
+			j++; // Incrémente l'indice de la colonne pour passer à la colonne suivante
+		}
+		write(1, "\n", 1); // Après avoir affiché tous les nombres de la ligne, ajoute un saut de ligne
+		i++; // Incrémente l'indice de la ligne pour passer à la ligne suivante
 	}
 }
